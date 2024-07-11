fd9a2ddc1f9d95a2a3d8b74d8ea84a5a456665f0
Mei Judith Difalco <100292459+judifalco@users.noreply.github.com>
2024-07-11 19:42:46 -0300

    Update autoresController.js

--- controllers/autoresController.js
+++ controllers/autoresController.js
@@ -25,37 +25,37 @@ function crearAutor(req, res) {
             res.status(500).send('Error al crear un nuevo autor');
             return;
         }
-        res.status(201).json({ message: 'Autor creado exitosamente', autorId: result.insertId });
+        res.status(201).json({ message: 'Autor creado exitosamente', id_autor: result.insertId });
     });
 }
 
 // Actualizar un autor existente
 function actualizarAutor(req, res) {
-    const autorId = req.params.id;
+    const id_autor = req.params.id;
     const { nombre, apellido, fecha_nacimiento } = req.body;
     const autorData = { nombre, apellido, fecha_nacimiento };
 
-    Autor.actualizarAutor(autorId, autorData, (err, result) => {
+    Autor.actualizarAutor(id_autor, autorData, (err, result) => {
         if (err) {
             console.error('Error al actualizar el autor:', err);
             res.status(500).send('Error al actualizar el autor');
             return;
         }
-        res.json({ message: 'Autor actualizado exitosamente', autorId: autorId });
+        res.json({ message: 'Autor actualizado exitosamente', id_autor: id_autor });
     });
 }
 
 // Eliminar un autor
 function eliminarAutor(req, res) {
-    const autorId = req.params.id;
+    const id_autor = req.params.id;
 
-    Autor.eliminarAutor(autorId, (err, result) => {
+    Autor.eliminarAutor(id_autor, (err, result) => {
         if (err) {
             console.error('Error al eliminar el autor:', err);
             res.status(500).send('Error al eliminar el autor');
             return;
         }
-        res.json({ message: 'Autor eliminado exitosamente', autorId: autorId });
+        res.json({ message: 'Autor eliminado exitosamente', id_autor: id_autor });
     });
 }
 
